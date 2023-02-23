9b2ef0fba0a74acf0f64fe25ab97cc997609cf09
vnovopashin <novopashinva@gmail.com>
2023-02-23 20:48:26 +0700

    1. Техническое задание - проект "АвтоМаг" [#504895]

---
+++ src/main/java/ru/job4j/cars/Job4jCarsApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job4jCarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/job4j/cars/Job4jCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
